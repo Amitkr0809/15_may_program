e0fe463b7465d75c1db07a3c59d41f9622f53b65
Amit Kumar <125899008+Amitkr0809@users.noreply.github.com>
2023-05-15 15:47:55 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,41 @@
+'''
+a = 22
+if (a > 9):
+     print("Greater")
+else:
+    print("Lesser")
+
+a = int (input("enter you age : "))
+if a>18:
+    print("you can vote")
+else:
+    print("you cannot vote")
+
+
+    
+'''
+
+
+
+marks = int(input("enter your mark : "))
+
+
+if marks>= 90:
+    grade = "A+"
+elif marks>= 80:
+    grade = "A"
+elif marks>= 70:
+    grade = "B"
+elif marks>= 60:
+    grade = "C"
+elif marks>= 50:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print ("your Grade is ", grade)
+
+
+
+
+
